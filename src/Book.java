2d472ef3b9d529bfae74e96397045c1bcc89d7c2
siddharth12-21 <siddharth.ramesh1221@gmail.com>
2025-10-28 16:27:01 +0000

    piglatin translation

--- src/Book.java
+++ src/Book.java
@@ -8,7 +8,26 @@ public class Book
 {
   public String pigLatin(String word)
   {
-
+    String word1 = word.toLowerCase();
+    int inx = 0;
+    String pigWord = "";
+    if (word1.charAt(0)=='a'||word1.charAt(0)=='e'||word1.charAt(0)=='i'||word1.charAt(0)=='o'||word1.charAt(0)=='u')
+    {
+      pigWord = word1.substring(1,word.length())+word.substring(0,1)+"yay";
+      return pigWord;
+    }
+    else
+    {
+      for (int i = 0; i<word1.length();i++){
+        if (word1.charAt(i)=='a'||word1.charAt(i)=='e'||word1.charAt(i)=='i'||word1.charAt(i)=='o'||word1.charAt(i)=='u'){
+          inx = i;
+          break;
+        }
+      }
+      int remain = word.length()-inx;
+      String piggy = word1.substring(inx)+word1.substring(0,inx)+"ay";
+      return piggy;
+    }
   }
   
   public int endPunctuation(String word)  //return the index of where the punctuation is at the end of a String. If it is all punctuation return 0, if there is no punctuation return -1
